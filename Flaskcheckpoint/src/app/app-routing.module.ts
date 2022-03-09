3c05138cd6e90c9fe5e1edf1be8f4fc252a82bf7
AlvaroMartinezSilva <60732646+AlvaroMartinezSilva@users.noreply.github.com>
2022-03-09 13:41:10 +0100

    ventanas arregladas

--- Flaskcheckpoint/src/app/app-routing.module.ts
+++ Flaskcheckpoint/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnalisisSentimientosComponent } from './components/analisis-sentimientos/analisis-sentimientos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path:'analisis-sentimientos', component: AnalisisSentimientosComponent
+
+},{
+  path:'footer', component: FooterComponent
+
+}
+,{
+  path:'navbar', component: NavbarComponent
+
+},{
+  path:'welcome', component: WelcomeComponent
+
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
