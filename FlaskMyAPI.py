6f88ce1e9e2ad4ba732b59bf17f8f6ef7993a46e
Jorge Diaz <jorge.dmm1@gmail.com>
2022-03-09 18:57:01 +0100

    fix CORs
    API de flask modificada para solucionar el error de CORs

--- FlaskMyAPI.py
+++ FlaskMyAPI.py
@@ -2,9 +2,11 @@
 
 #from urllib import request
 from flask import Flask, jsonify, request, abort, make_response
+from flask_cors import CORS, cross_origin#CORs $pip install -U flask-cors
 
 #Bucle del servidor
 app = Flask(__name__)
+CORS(app)#CORs
 
 #funciones personalizadas
 
@@ -37,6 +39,9 @@ def sentimental():
     }
     return jsonify( {'analisis': analisis })
 
+@app.route('/saludo', methods = ['GET'])
+def saludo():
+    return jsonify( {'saludo': 'Hola, ya estoy conectada' })
 
 #Pordefecto
 @app.route('/', methods = ['GET'])
@@ -47,3 +52,5 @@ def inicio():
 if __name__ == '__main__':
     app.run(debug = True)
     #app.run(debug = True, host='0.0.0.0', port=80)
+
+#Para el CORs https://stackoverflow.com/questions/28461001/python-flask-cors-issue#:~:text=Flask%20has%20the%20flask%2Dcors%20module.%20Following%20is%20the%20code%20snippet%20as%20well%20as%20the%20procedure.
