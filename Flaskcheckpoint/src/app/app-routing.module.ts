9204f7ca6fcf0240c721d487cf8c3c0706768429
Nico-j-j <nicolekuylen65@gmail.com>
2022-03-12 15:35:28 +0100

    arreglos

--- Flaskcheckpoint/src/app/app-routing.module.ts
+++ Flaskcheckpoint/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 path:'analisis-sentimientos', component: AnalisisSentimientosComponent
 
 },{
-  path:'welcome', component: WelcomeComponent
+  path:'', component: WelcomeComponent
 
 },{
 
